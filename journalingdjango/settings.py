abb9e861a84b313b4ca69a5eed119252e01fb646
matthew-f-bell <xdjx3runx@gmail.com>
2025-08-05 17:07:48 -0700

    New DB connection

--- journalingdjango/settings.py
+++ journalingdjango/settings.py
@@ -14,6 +14,9 @@ import dj_database_url
 
 load_dotenv()
 
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -38,7 +41,7 @@ else:
 if DJANGO_HOST == "production":
     DEBUG = False
 else:
-    DEBUG = True
+    DEBUG = False
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
@@ -104,6 +107,8 @@ DATABASES = {
         'NAME': os.getenv("DJANGO_DATABASE_NAME"),
     }
 }
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
