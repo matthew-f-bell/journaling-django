9a7e1307b4f1038bf96435da335f9f24e3fc7218
matthew-f-bell <xdjx3runx@gmail.com>
2025-08-05 17:41:26 -0700

    Reset Static URL

--- journalingdjango/settings.py
+++ journalingdjango/settings.py
@@ -144,10 +144,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-# STATIC_URL = '/static/'
 # Set Deployment Static Root
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
