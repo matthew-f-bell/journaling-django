dff4260af4752e6db519028e5472abd9d042a8ee
matthew-f-bell <xdjx3runx@gmail.com>
2025-08-06 20:00:51 -0700

    Changed Static Root

--- journalingdjango/settings.py
+++ journalingdjango/settings.py
@@ -146,8 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 # Set Deployment Static Root
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
